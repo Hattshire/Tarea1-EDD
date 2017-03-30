1074ceee8fd217849c6b384d50d1e975d7fda269
Hattshire <julian.sepulveda@sansano.usm.cl>
2017-03-29 22:50:38 -0300

    Agregar comentarios al archivo

--- main.c
+++ main.c
@@ -4,19 +4,39 @@
 int
 main( void )
 {
+    // Abrir archivo strings.txt
     FILE * Fstrings = fopen( "strings.txt", "r" );
+    // TODO: verificar existencia del archivo
+    
+    // Declarar e inicializar contadores
+    // n = tamanyo del array strings
+    // l = linea actual en el archivo ( que corresponda a las string )
+    // c = cuantos int ha encontrado
     int n = 2, l = 0, c = 0;
+    
+    // Declarar el array strings y asignar espacio basico
     char** strings = (char**)malloc( n * sizeof(char*) );
+    
+    // Declarar el array counts que contiene los ints/contadores del archivo
     int counts[2];
-    while ( fscanf( Fstrings, "%s", strings[l] ) || ( fscanf( Fstrings, "%d", counts[c] ) && c++ < 2 ) )
+    
+    // Procesar el archivo
+    while ( fscanf( Fstrings, "%s", strings[l] ) || ( fscanf( Fstrings, "%d", counts[c] ) && c++ < 2 && l-- ) )
     {
         l++;
+        // Verificar si el array tiene espacio suficiente para los proximos elementos
         if ( l == n )
         {
+            // Duplicar el espacio del array
             n *= 2;
             strings = (char**)realloc( strings, n * sizeof(char*) );
+            // TODO: Evitar el sobrellenado de memoria
         }
     }
+    
+    // Cerrar el archivo
     fclose( Fstrings );
+    
+    // Retorno exitoso
     return EXIT_SUCCESS;
 }
