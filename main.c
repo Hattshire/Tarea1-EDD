100d7365b95190e7d5cd3ea296368641d8bfab7f
Hattshire <julian.sepulveda@sansano.usm.cl>
2017-03-29 22:40:56 -0300

    Integrar contadores

--- main.c
+++ main.c
@@ -5,9 +5,10 @@ int
 main( void )
 {
     FILE * Fstrings = fopen( "strings.txt", "r" );
-    int n = 2, l = 0;
+    int n = 2, l = 0, c = 0;
     char** strings = (char**)malloc( n * sizeof(char*) );
-    while ( fscanf( Fstrings, "%s", strings[l] ) )
+    int counts[2];
+    while ( fscanf( Fstrings, "%s", strings[l] ) || ( fscanf( Fstrings, "%d", counts[c] ) && c++ < 2 ) )
     {
         l++;
         if ( l == n )
