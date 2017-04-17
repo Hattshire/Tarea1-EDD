601e9b750093df16b351427df22477376516557b
Hattshire <julian.sepulveda@sansano.usm.cl>
2017-04-16 22:49:43 -0300

    falto poner null

--- sintil.de.fn.c
+++ sintil.de.fn.c
@@ -3,52 +3,54 @@
 void
 sintilde(char* palabra)
 {
-        for ( int j = 0, k = 0 ;palabra[j] != 0  ; j++, k++ )
+    int j = 0, k = 0;
+    for ( ;palabra[j] != 0  ; j++, k++ )
+    {
+        if ( palabra[j] == '\303' )
         {
-            if ( palabra[j] == '\303' )
-            {
-                k--;
-                continue;
-            }                
-            if ( palabra[j] >= ((char*)"à")[1] &&
-                 palabra[j] <= ((char*)"æ")[1] )
-                palabra[k] = 'a';
-                
-            else if ( palabra[j] >= ((char*)"ì")[1] &&
-                      palabra[j] <= ((char*)"ï")[1] )
-                palabra[k] = 'i';
-                
-            else if ( palabra[j] >= ((char*)"ù")[1] &&
-                      palabra[j] <= ((char*)"ü")[1] )
-                palabra[k] = 'u';
-                
-            else if ( palabra[j] >= ((char*)"è")[1] &&
-                      palabra[j] <= ((char*)"ë")[1] )
-                palabra[k] = 'e';
-                
-            else if ( palabra[j] >= ((char*)"ò")[1] &&
-                      palabra[j] <= ((char*)"ö")[1] )
-                palabra[k] = 'o';
-                  
-            else if ( palabra[j] >= ((char*)"À")[1] &&
-                     palabra[j] <= ((char*)"Ä")[1] )
-                palabra[k] = 'a';
-            else if ( palabra[j] >= ((char*)"Ì")[1] &&
-                      palabra[j] <= ((char*)"Ï")[1] )
-                palabra[k] = 'i';
-            else if ( palabra[j] >= ((char*)"Ù")[1] &&
-                      palabra[j] <= ((char*)"Ü")[1] )
-                palabra[k] = 'u';
-            else if ( palabra[j] >= ((char*)"È")[1] &&
-                      palabra[j] <= ((char*)"Ë")[1] )
-                palabra[k] = 'e';
-            else if ( palabra[j] >= ((char*)"Ò")[1] &&
-                      palabra[j] <= ((char*)"Ö")[1] )
-                palabra[k] = 'o';
-            else if ( palabra[j] == "ñ"[1] )
-                palabra[k] = 'n';
-            else if ( palabra[j] == "Ñ"[1] )
-                palabra[k] = 'n';
-            else palabra[k] = tolower(palabra[j]);
+            k--;
+            continue;
         }
+        if ( palabra[j] >= "à"[1] &&
+             palabra[j] <= "æ"[1] )
+            palabra[k] = 'a';
+            
+        else if ( palabra[j] >= "ì"[1] &&
+                  palabra[j] <= "ï"[1] )
+            palabra[k] = 'i';
+            
+        else if ( palabra[j] >= "ù"[1] &&
+                  palabra[j] <= "ü"[1] )
+            palabra[k] = 'u';
+            
+        else if ( palabra[j] >= "è"[1] &&
+                  palabra[j] <= "ë"[1] )
+            palabra[k] = 'e';
+            
+        else if ( palabra[j] >= "ò"[1] &&
+                  palabra[j] <= "ö"[1] )
+            palabra[k] = 'o';
+              
+        else if ( palabra[j] >= "À"[1] &&
+                 palabra[j] <= "Ä"[1] )
+            palabra[k] = 'a';
+        else if ( palabra[j] >= "Ì"[1] &&
+                  palabra[j] <= "Ï"[1] )
+            palabra[k] = 'i';
+        else if ( palabra[j] >= "Ù"[1] &&
+                  palabra[j] <= "Ü"[1] )
+            palabra[k] = 'u';
+        else if ( palabra[j] >= "È"[1] &&
+                  palabra[j] <= "Ë"[1] )
+            palabra[k] = 'e';
+        else if ( palabra[j] >= "Ò"[1] &&
+                  palabra[j] <= "Ö"[1] )
+            palabra[k] = 'o';
+        else if ( palabra[j] == "ñ"[1] )
+            palabra[k] = 'n';
+        else if ( palabra[j] == "Ñ"[1] )
+            palabra[k] = 'n';
+        else palabra[k] = tolower(palabra[j]);
     }
+    palabra[k] = 0;
+}
